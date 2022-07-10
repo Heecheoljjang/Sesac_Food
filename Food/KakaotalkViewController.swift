5f6f3820bd0cae7b32824eacf0143ce09d931229
Heecheoljjang <kkll135@naver.com>
2022-07-10 23:52:17 +0900

    카카오톡 UI 뼈대 구성

---
+++ Food/KakaotalkViewController.swift
@@ -0,0 +1,31 @@
+//
+//  KakaotalkViewController.swift
+//  Food
+//
+//  Created by HeecheolYoon on 2022/07/10.
+//
+
+import UIKit
+
+class KakaotalkViewController: UIViewController {
+    
+    @IBOutlet var backgroundView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backgroundView.backgroundColor = .
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
